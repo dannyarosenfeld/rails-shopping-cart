c12ba6e58e02a45aab0f7f69040595513fb00c12
dbcbrianchung <brianjaychung@gmail.com>
2016-06-13 16:04:35 -0700

    Add private item_params method and create and new methods.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,33 @@
 class ItemsController < ApplicationController
-  def new
-    # @items = Item.all
+  def index
+    @items = Item.all
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :text, :price)
+  end
+
 end
+
