058b87520b9cb1b7f26a443d0de8c031beae019f
dbcbrianchung <brianjaychung@gmail.com>
2016-06-14 14:56:39 -0700

    test written for update and delete

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -20,4 +20,21 @@ RSpec.feature "Items", type: :feature, :js => true do
       expect(page).to have_content(item.text)
     end
   end
+  describe "item page" do
+    before(:each) do
+      visit edit_item_path(item.id)
+    end
+    it "displays a form and updates an existing post" do
+      new_title = "Test updated title"
+      fill_in('Title', with: new_title)
+      click_on('Save Item')
+      expect(page).to have_content(new_title)
+    end
+    it "has a button that deletes the article" do
+      visit root_path(item.id)
+      click_on('Delete')
+      page.accept_alert
+      expect(page).to_not have_content(item.title)
+    end
+  end
 end
