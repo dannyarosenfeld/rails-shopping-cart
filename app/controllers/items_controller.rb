a9c2b1b3968149a292b7cd706aae88ebc4718b4f
dbcbrianchung <brianjaychung@gmail.com>
2016-06-13 17:31:35 -0700

    Change delete to destroy in methods.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,14 +14,32 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    @item.save
-    redirect_to @item
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def delete
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
   end
 
   private
