d32df37f6e071358be10864961af3615d1382bb2
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-06-13 22:35:43 -0700

    Refactor validation.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,3 @@
 class Item < ActiveRecord::Base
-	validates :title, presence: true
-	validates :text, presence: true
-	validates :price, presence: true
-	validates :image, presence: true
+	validates :title, :text, :price, :image, presence: true
 end
