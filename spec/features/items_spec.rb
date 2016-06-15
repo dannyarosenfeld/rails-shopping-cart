20c0af113e3d620c86ca240cbec01bcf8f56549b
dbcbrianchung <brianjaychung@gmail.com>
2016-06-15 10:37:35 -0700

    Modify delete test, change route since page was changed in design.

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -13,13 +13,13 @@ RSpec.feature "Items", type: :feature, :js => true do
     describe "#create" do
       it "fills in text in the form and renders item page" do
         visit new_item_path
-        fill_in "Item", :with => item.title
-        fill_in "Description", :with => item.text
-        fill_in "Price", :with => item.price
-        fill_in "Image", :with => item.image
-        fill_in "Quantity", :with => item.quantity
+        fill_in "item[title]", :with => item.title
+        fill_in "item[text]", :with => item.text
+        fill_in "item[price]", :with => item.price
+        fill_in "item[image]", :with => item.image
+        fill_in "item[quantity]", :with => item.quantity
         click_button "Save Item"
-        expect(page).to have_content(item.text)
+        expect(page).to have_content(item.price)
       end
     end
     describe "item page" do
@@ -28,12 +28,12 @@ RSpec.feature "Items", type: :feature, :js => true do
       end
       it "displays a form and updates an existing post" do
         new_title = "Test updated title"
-        fill_in('Item', with: new_title)
+        fill_in('item[title]', with: new_title)
         click_on('Save Item')
         expect(page).to have_content(new_title)
       end
       it "has a button that deletes the article" do
-        visit root_path(item.id)
+        visit item_path(item.id)
         click_on('Delete')
         page.accept_alert
         expect(page).to_not have_content(item.title)
