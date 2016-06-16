76171268026d307691ef02e5d8184ed88f41b578
dbcbrianchung <brianjaychung@gmail.com>
2016-06-15 17:27:57 -0700

    admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,31 @@
+require_relative '../helpers/sessions_helper'
+
 class SessionsController < ApplicationController
+
+  include SessionsHelper
+
   def new
   end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(username: params[:session][:username].downcase)
+
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.username == "tim@tim.com"
+      redirect_to admin_path
+      end
+      log_in(@user)
+      flash[:success] = 'You are logged in!'
       redirect_to items_path
     else
-      flash.now[:danger] = 'Invalid username/password, please try again'
-      render 'new'
+      flash[:danger] = 'Invalid username/password, please try again'
+      redirect_to login_path
     end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = 'You are logged out!'
+    redirect_to items_path
   end
 end
